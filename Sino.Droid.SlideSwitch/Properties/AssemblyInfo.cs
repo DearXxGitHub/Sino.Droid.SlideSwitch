2af208c198c4deb0d14e6c3e4fae6fea35356e0c
y-z-f <976691141@qq.com>
2016-04-12 10:08:46 +0800

    初始化项目

---
+++ Sino.Droid.SlideSwitch/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sino.Droid.SlideSwitch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sino")]
+[assembly: AssemblyProduct("Sino.Droid.SlideSwitch")]
+[assembly: AssemblyCopyright("Copyright © Sino 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
---
+++ SlideSwitch.Sample/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace SlideSwitch.Sample
+{
+    [Activity(Label = "SlideSwitch.Sample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
